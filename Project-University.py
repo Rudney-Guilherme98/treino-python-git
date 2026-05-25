5550498f8da325cc8a0566f42994eeec55b4c39b
Rudney Macedo <rudney.guilherme98@gmail.com>
2026-05-25 16:16:04 -0300

    Estou aprendendo um pouco de python e treinando tambem alguns comando no git bash

--- Project-University.py
+++ Project-University.py
@@ -20,8 +20,20 @@ class Aventureiro:
 # 1. Criando o herói 
 heroi = Aventureiro("Rudney")
 
-# 2. Chamando a ação de treinar duas vezes
-heroi.treinar()
-heroi.treinar()
-
-# toda vez que eu comecar as 
+# 2. O Loop Infinito (Lê-se: "Enquanto for Verdadeiro, faça...")
+while True:
+    
+    # 3. Capturando a escolha do usuário e guardando na variável 'opcao'
+    opcao = input("Digite 1 para Treinar ou 2 para Sair: ")
+    
+    # 4. Verificando a escolha (Note os dois sinais de igual '==' para comparar!)
+    if opcao == "1":
+        heroi.treinar() # Chama a ação da fábrica! O print já está lá dentro.
+        
+    elif opcao == "2":
+        print("Fim de jogo!")
+        break # A palavra mágica que 'quebra' e encerra o loop infinito
+        
+    else:
+        # Um bônus: se ele digitar '3' ou letras sem querer!
+        print("Opção inválida, tente novamente.")
